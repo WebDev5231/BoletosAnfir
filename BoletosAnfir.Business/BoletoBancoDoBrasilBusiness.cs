fd01f65a93894946986177b8d9ffd615d66a3aee
Vinicius Gomes <viniciusgarciagomes@hotmail.com>
2023-09-26 11:56:44 -0300

    Finalizando Boleto do BB
    Finalizando Boleto do BB

--- BoletosAnfir.Business/BoletoBancoDoBrasilBusiness.cs
+++ BoletosAnfir.Business/BoletoBancoDoBrasilBusiness.cs
@@ -2,6 +2,7 @@
 using BoletosAnfir.Model;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,96 +11,50 @@ namespace BoletosAnfir.Business
 {
     public class BoletoBancoDoBrasilBusiness
     {
-        public string GeraHtmlBoleto(BoletoAnfir boleto)
+        public string GeraHtmlBoleto(BoletoAnfir boleto, DadosCedenteBB dadosCedente)
         {
-            DateTime vencimento = DateTime.Now.AddDays(5);
-
-            #region Exemplo Carteira 16, com nosso número de 11 posições
-            /*
-             * Nesse exemplo utilizamos a carteira 16 e o nosso número no máximo de 11 posições.
-             * Não é necessário informar o numero do convênio e nem o tipo da modalidade.
-             * O nosso número tem que ter no máximo 11 posições.
-             */
-
-            Cedente c = new Cedente("00.000.000/0000-00", "Empresa de Atacado", "1234", "1", "123456", "1");
-            Boleto b = new Boleto(vencimento, 0.01m, "16", "09876543210", c);
-
-            #endregion Exemplo Carteira 16, com nosso número de 11 posições
-
-            #region Exemplo Carteira 16, convênio de 6 posições e tipo modalidade 21
-            /*
-             * Nesse exemplo utilizamos a carteira 16 e o número do convênio de 6 posições.
-             * É obrigatório informar o tipo da modalidade 21.
-             * O nosso número tem que ter no máximo 10 posições.
-             */
-
-            //Cedente c = new Cedente("00.000.000/0000-00", "Empresa de Atacado", "1234", "1", "123456", "1");
-            //c.Convenio = 123456;
-
-            //Boleto b = new Boleto(vencimento, 0.01, "16", "09876543210", c);
-            //b.TipoModalidade = "21";
-            #endregion Exemplo Carteira 16, convênio de 6 posições e tipo modalidade 21
-
-            #region Exemplo Carteira 18, com nosso número de 11 posições
-            /*
-             * Nesse exemplo utilizamos a carteira 18 e o nosso número no máximo de 11 posições.
-             * Não é necessário informar o numero do convênio e nem o tipo da modalidade.
-             * O nosso número tem que ter no máximo 11 posições.
-             */
-
-            //Cedente c = new Cedente("00.000.000/0000-00", "Empresa de Atacado", "1234", "1", "123456", "1");
-            //Boleto b = new Boleto(vencimento, 0.01, "18", "09876543210", c);
-
-            #endregion Exemplo Carteira 18, com nosso número de 11 posições
-
-            #region Exemplo Carteira 18, convênio de 6 posições e tipo modalidade 21
-            /*
-             * Nesse exemplo utilizamos a carteira 18 e o número do convênio de 6 posições.
-             * É obrigatório informar o tipo da modalidade 21.
-             * O nosso número tem que ter no máximo 10 posições.
-             */
-
-            //Cedente c = new Cedente("00.000.000/0000-00", "Empresa de Atacado", "1234", "1", "123456", "1");
-            //c.Convenio = 123456;
-            //Boleto b = new Boleto(vencimento, 0.01, "18", "09876543210", c);
-            //b.TipoModalidade = "21";
-            #endregion Exemplo Carteira 18, convênio de 6 posições e tipo modalidade 21
-
-
-            b.NumeroDocumento = "12415487";
-
-            b.Sacado = new Sacado("000.000.000-00", "Nome do seu Cliente ");
-            b.Sacado.Endereco.End = "Endereço do seu Cliente ";
-            b.Sacado.Endereco.Bairro = "Bairro";
-            b.Sacado.Endereco.Cidade = "Cidade";
-            b.Sacado.Endereco.CEP = "00000000";
-            b.Sacado.Endereco.UF = "UF";
-
-            //Adiciona as instruções ao boleto
-            #region Instruções
-            //Protestar
-            Instrucao_BancoBrasil item = new Instrucao_BancoBrasil(9, 5);
-            b.Instrucoes.Add(item);
-            //ImportanciaporDiaDesconto
-            item = new Instrucao_BancoBrasil(30, 0);
-            b.Instrucoes.Add(item);
-            //ProtestarAposNDiasCorridos
-            item = new Instrucao_BancoBrasil(81, 15);
-            b.Instrucoes.Add(item);
-            #endregion Instruções
-
             var boletoBancario = new BoletoBancario();
+            boletoBancario.CodigoBanco = 001;
+
+            var dataVencimento = boleto.Data_Venc;
+
+            Instrucao_BancoBrasil item = new Instrucao_BancoBrasil(9, 5);
+
+            Cedente c = new Cedente(dadosCedente.CNPJ, dadosCedente.Empresa, dadosCedente.Agencia, dadosCedente.DigitoAgencia, dadosCedente.NumeroConta, dadosCedente.DigitoConta);
+            c.Codigo = "";
+
+            Boleto b = new Boleto(dataVencimento, 1.01m, dadosCedente.NumeroCarteira, boleto.Nosso_Numero, c);
+
+            b.Banco = new Banco(001);
+            b.NumeroDocumento = boleto.Numero_Doc;
+            b.DataVencimento = dataVencimento;
+            b.ValorBoleto = Math.Round(boleto.Valor, 2);
+            b.DataDocumento = boleto.Data_Doc;
+
+            b.Sacado = new Sacado(boleto.CNPJ, boleto.Razao);
+            b.Sacado.Endereco.End = boleto.Endereco;
+            b.Sacado.Endereco.Cidade = boleto.munMUNICIP;
+            b.Sacado.Endereco.CEP = boleto.Cep;
+            b.Sacado.Endereco.UF = boleto.munEST;
+
+            Instrucao i = new Instrucao(1);
+
+            var dataContribuicao = boleto.Data_Doc;
+            i.Descricao = $"TAXA ASSOCIATIVE EXTRA " + dataContribuicao.ToString("MMMM DE yyyy", new CultureInfo("pt-BR")).ToUpper();
+
+            b.Instrucoes.Add(i);
+            b.EspecieDocumento = new EspecieDocumento_BancoBrasil("4");
+            b.Carteira = dadosCedente.NumeroCarteira;
+            b.NumeroDocumento = boleto.Numero_Doc;
+            c.Convenio = dadosCedente.convenio;
+
+            boletoBancario.MostrarContraApresentacaoNaDataVencimento = false;
 
             boletoBancario.Boleto = b;
             boletoBancario.Boleto.Valida();
 
-            boletoBancario.AjustaTamanhoFonte(12, tamanhoFonteInstrucaoImpressao: 14);
-
-            boletoBancario.OcultarInstrucoes = true;
-            //boletoBancario.FormatoPropaganda = (Request.Url.Query == "?formatopropaganda");
-            //boletoBancario.ImagemPropaganda = GetBase64StringForImage(Server.MapPath("/prop.jpg"));
-
             var html = boletoBancario.MontaHtml();
+            html = $"<html><head><meta charset='UTF-8'></head><body><div>{html}</div></body></html>";
             return html;
         }
     }
--- BoletosAnfir.Business/BoletoBradescoBusiness.cs
+++ BoletosAnfir.Business/BoletoBradescoBusiness.cs
@@ -2,6 +2,7 @@
 using BoletosAnfir.Model;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,51 +11,44 @@ namespace BoletosAnfir.Business
 {
     public class BoletoBradescoBusiness
     {
-        public string GeraHtmlBoleto(BoletoAnfir boleto)
+        public string GeraHtmlBoleto(BoletoAnfir boleto, DadosCedenteBradesco dadosCedente)
         {
             var boletoBancario = new BoletoBancario();
             boletoBancario.CodigoBanco = 237;
 
-            DateTime vencimento = DateTime.Now.AddDays(5);
+            var dataVencimento = boleto.Data_Venc;
 
             Instrucao_Bradesco item = new Instrucao_Bradesco(9, 5);
 
-            Cedente c = new Cedente("00.000.000/0000-00", "Empresa de Atacado", "1234", "5", "123456", "7");
-            c.Codigo = "13000";
+            Cedente c = new Cedente(dadosCedente.CNPJ, dadosCedente.Empresa, dadosCedente.Agencia, dadosCedente.DigitoAgencia, dadosCedente.NumeroConta, dadosCedente.DigitoConta);
+            c.Codigo = "";
 
-            //Carteiras 
-            Boleto b = new Boleto(vencimento, 1.01m, "09", "01000000001", c);
+            Boleto b = new Boleto(dataVencimento, 1.01m, dadosCedente.NumeroCarteira, boleto.Numero_Doc, c);
+
             b.Banco = new Banco(237);
-            b.NumeroDocumento = "01000000001";
+            b.NumeroDocumento = boleto.Nosso_Numero;
+            b.DataVencimento = dataVencimento;
+            b.ValorBoleto = Math.Round(boleto.Valor, 2);
+            b.DataDocumento = boleto.Data_Doc;
 
-            b.Sacado = new Sacado("000.000.000-00", "Nome do seu Cliente ");
-            b.Sacado.Endereco.End = "Endereço do seu Cliente ";
-            b.Sacado.Endereco.Bairro = "Bairro";
-            b.Sacado.Endereco.Cidade = "Cidade";
-            b.Sacado.Endereco.CEP = "00000000";
-            b.Sacado.Endereco.UF = "UF";
-
-            item.Descricao += " após " + item.QuantidadeDias.ToString() + " dias corridos do vencimento.";
-            b.Instrucoes.Add(item); //"Não Receber após o vencimento");
+            b.Sacado = new Sacado(boleto.CNPJ, boleto.Razao);
+            b.Sacado.Endereco.End = boleto.Endereco;
+            b.Sacado.Endereco.Cidade = boleto.munMUNICIP;
+            b.Sacado.Endereco.CEP = boleto.Cep;
+            b.Sacado.Endereco.UF = boleto.munEST;
 
             Instrucao i = new Instrucao(237);
-            i.Descricao = "Nova Instrução";
+
+            var dataContribuicao = boleto.Data_Doc;
+            i.Descricao = $"CONTRIBUIÇÃO ASSOCIATIVA " + dataContribuicao.ToString("MMMM DE yyyy", new CultureInfo("pt-BR")).ToUpper();
+
             b.Instrucoes.Add(i);
-
             b.EspecieDocumento = new EspecieDocumento_Bradesco("01");
+            b.Carteira = dadosCedente.NumeroCarteira;
+            b.NumeroDocumento = boleto.Nosso_Numero;
 
-            /* 
-             * A data de vencimento não é usada
-             * Usado para mostrar no lugar da data de vencimento o termo "Contra Apresentação";
-             * Usado na carteira 06
-             */
-            boletoBancario.MostrarContraApresentacaoNaDataVencimento = true;
+            boletoBancario.MostrarContraApresentacaoNaDataVencimento = false;
 
-            var valorBoleto = 400;
-            var carteira = "1234";
-            var numeroDocumento = "12345";
-
-            boletoBancario.Boleto = new Boleto(vencimento, valorBoleto, carteira, numeroDocumento, c);
             boletoBancario.Boleto = b;
             boletoBancario.Boleto.Valida();
 
